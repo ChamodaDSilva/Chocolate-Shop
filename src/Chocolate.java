f793bac78caaf6a4803dfb003f3ab8151373e164
chamoda de silva <chamoda12345678@gmail.com>
2022-05-23 09:20:59 +0530

    added chocolate class

---
+++ src/Chocolate.java
@@ -0,0 +1,13 @@
+public class Chocolate {
+    private int chocolateId;
+    private int ChocolateType;
+    private String brand;
+    private String type;
+    private double price;
+    private double discount;
+    private double weight;
+    private int quantityInStock;
+    private int quantitySold;
+
+
+}
